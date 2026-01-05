7d56a9590b425977e5272a1aca91db1c6995ff34
Juwon-hwang <57760393+Juwon-hwang@users.noreply.github.com>
2026-01-05 16:24:40 +0900

    Add files via upload

---
+++ infra/terraform/iam.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "cloudship-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "cloudship-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
