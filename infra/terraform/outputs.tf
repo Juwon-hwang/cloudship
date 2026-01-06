7ff26896a2852a08a1f57b6e27dc927306c6e32f
Juwon Hwang <juwon2588@naver.com>
2026-01-06 14:28:28 +0900

    infra: output instance_id to pin SSM deploy target

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "ec2_public_ip" {
-  value = aws_instance.app.public_ip
+output "instance_id" {
+  value = aws_instance.app.id
 }
 
-output "ec2_public_dns" {
-  value = aws_instance.app.public_dns
+output "ec2_public_ip" {
+  value = aws_instance.app.public_ip
 }
